72fa6281cd5c107439e2498bb7d9beefd512a221
unknown <criley1@7GXYYN1.olin.edu>
2015-01-25 19:52:00 -0500

    blinking

--- blink/blink.c
+++ blink/blink.c
@@ -10,16 +10,26 @@ int16_t main(void) {
     init_timer();
 
     led_on(&led1);
-    timer_setPeriod(&timer2, 0.5);
+    led_on(&led2);
+    led_on(&led3);
+    timer_setPeriod(&timer2, 0.1);
     timer_start(&timer2);
+
+    int loop_counter = 0;
 
     while (1) {
         if (timer_flag(&timer2)) {
             timer_lower(&timer2);
-            led_toggle(&led1);
+            loop_counter ++;
+            if (loop_counter%1 == 0) {
+                led_toggle(&led1);}
+            if (loop_counter%5 == 0) {
+                led_toggle(&led2);}
+            if (loop_counter%10 == 0) {
+                led_toggle(&led3);}
+            if (loop_counter == 20) {
+                loop_counter = 0;}
         }
-        led_write(&led2, !sw_read(&sw2));
-        led_write(&led3, !sw_read(&sw3));
     }
 }
 
