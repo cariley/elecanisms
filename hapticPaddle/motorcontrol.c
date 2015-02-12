4d6fe847d5e7b34a1a766f7f81113563c8234eae
unknown <criley1@7GXYYN1.olin.edu>
2015-02-12 12:03:13 -0500

    serial read

--- hapticPaddle/motorcontrol.c
+++ hapticPaddle/motorcontrol.c
@@ -1,5 +1,6 @@
 #include <p24FJ128GB206.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "config.h"
 #include "common.h"
 #include "ui.h"
@@ -7,52 +8,20 @@
 #include "pin.h"
 #include "oc.h"
 #include "uart.h"
-#include "usb.h"
 
-#define HELLO       0   // Vendor request that prints "Hello World!"
-#define SET_VALS    1   // Vendor request that receives 2 unsigned integer values
-#define GET_VALS    2   // Vendor request that returns 2 unsigned integer values
-#define PRINT_VALS  3   // Vendor request that prints 2 unsigned integer values 
-
-uint16_t angleVal;
-
-void VendorRequests(void) {
-    WORD temp;
-
-    switch (USB_setup.bRequest) {
-        case HELLO:
-            printf("Hello World!\n");
-            BD[EP0IN].bytecount = 0;    // set EP0 IN byte count to 0 
-            BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
-            break;
-        case GET_VALS:
-            temp.w = angleVal;
-            BD[EP0IN].address[0] = temp.b[0];
-            BD[EP0IN].address[1] = temp.b[1];
-            //temp.w = val2;
-            //BD[EP0IN].address[2] = temp.b[0];
-            //BD[EP0IN].address[3] = temp.b[1];
-            BD[EP0IN].bytecount = 2;    // set EP0 IN byte count to 4
-            BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
-            break;            
-        default:
-            USB_error_flags |= 0x01;    // set Request Error Flag
-    }
-}
-
-void VendorRequestsIn(void) {
-    switch (USB_request.setup.bRequest) {
-        default:
-            USB_error_flags |= 0x01;                    // set Request Error Flag
-    }
-}
-
-void VendorRequestsOut(void) {
-    switch (USB_request.setup.bRequest) {
-        default:
-            USB_error_flags |= 0x01;                    // set Request Error Flag
-    }
-}
+// Position tracking variables
+long long updatedPos;     // keeps track of the latest updated value of the MR sensor reading
+long long rawPos;         // current raw reading from MR sensor
+long long lastRawPos;     // last raw reading from MR sensor
+long long lastLastRawPos; // last last raw reading from MR sensor
+int16_t flipNumber;     // keeps track of the number of flips over the 180deg mark
+uint16_t tempOffset;
+uint16_t rawDiff;
+uint16_t lastRawDiff;
+uint16_t rawOffset;
+uint16_t lastRawOffset;
+uint16_t flipThresh = 700;  // threshold to determine whether or not a flip over the 180 degree mark occurred
+bool flipped = false;
 
 int16_t main(void) {
     uint16_t btn2ReadState;
@@ -65,9 +34,8 @@ int16_t main(void) {
     init_pin();
     init_oc();
     init_uart();
-    InitUSB();
 
-    pin_analogIn(&A[5]);
+    pin_analogIn(&A[0]);
 
     pin_digitalOut(&D[2]);
     pin_set(&D[2]);
@@ -81,22 +49,47 @@ int16_t main(void) {
     pin_digitalOut(&D[5]);
     pin_clear(&D[5]);
 
-    pin_digitalOut(&D[6]);
-
     pin_digitalOut(&D[7]);
     pin_set(&D[7]);
 
+    pin_digitalOut(&D[6]);
     oc_pwm(&oc1, &D[6], &timer2, 20000, 30000);
 
-    while (USB_USWSTAT!=CONFIG_STATE) {     
-        ServiceUSB();                       
-    }
+    lastLastRawPos = pin_read(&A[0]);
+    lastRawPos = pin_read(&A[0]);
 
     while(1){
         btn2ReadState = !sw_read(&sw2);
 
-        angleVal = pin_read(&A[5]);     
-        ServiceUSB(); 
+        rawPos = pin_read(&A[0]); 
+
+        rawDiff = rawPos - lastRawPos;          
+        lastRawDiff = rawPos - lastLastRawPos; 
+        rawOffset = abs(rawDiff);
+        lastRawOffset = abs(lastRawDiff);
+        
+        lastLastRawPos = lastRawPos;
+        lastRawPos = rawPos;
+          
+        if((lastRawOffset > flipThresh) && (!flipped)) { 
+            if(lastRawDiff > 0) {        
+                flipNumber--;             
+            } else {                     
+                flipNumber++;             
+            }
+            if(rawOffset > flipThresh) { 
+                updatedPos = rawPos + flipNumber*rawOffset; 
+                tempOffset = rawOffset;
+            } else {               
+                updatedPos = rawPos + flipNumber*lastRawOffset;  
+            }
+            flipped = true;          
+        } else {                       
+            updatedPos = rawPos + flipNumber*tempOffset;
+            flipped = false;
+        }
+
+        printf("%d   :   %d\n",rawPos,updatedPos);
 
         if (btn2CurrState != btn2ReadState) {
             counter2++;
