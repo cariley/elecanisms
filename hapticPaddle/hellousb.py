b2976d3ca26cd90f6bd3575268937eed9a68518f
unknown <criley1@7GXYYN1.olin.edu>
2015-02-13 16:46:44 -0500

    sample then print

--- hapticPaddle/hellousb.py
+++ hapticPaddle/hellousb.py
@@ -1,13 +1,18 @@
 import serial
+import os
 
 ser = serial.Serial(
-    port='COM5',\
+    port='COM4',\
     baudrate=19200,\
     parity=serial.PARITY_NONE,\
     stopbits=serial.STOPBITS_ONE,\
     bytesize=serial.EIGHTBITS,\
         timeout=0)
 
+f = open("values.txt","w+")
 while True:
     line = ser.readline()
     print line
+    f.write(line)
+    f.flush();
+    os.fsync(f.fileno())
--- hapticPaddle/motorcontrol.c
+++ hapticPaddle/motorcontrol.c
@@ -10,23 +10,26 @@
 #include "uart.h"
 
 // Position tracking variables
-long long updatedPos;     // keeps track of the latest updated value of the MR sensor reading
-long long rawPos;         // current raw reading from MR sensor
-long long lastRawPos;     // last raw reading from MR sensor
-long long lastLastRawPos; // last last raw reading from MR sensor
+int16_t rawPos;         // current raw reading from MR sensor
+int16_t lastRawPos;     // last raw reading from MR sensor
+int16_t lastLastRawPos; // last last raw reading from MR sensor
 int16_t flipNumber;     // keeps track of the number of flips over the 180deg mark
-uint16_t tempOffset;
-uint16_t rawDiff;
-uint16_t lastRawDiff;
-uint16_t rawOffset;
-uint16_t lastRawOffset;
-uint16_t flipThresh = 700;  // threshold to determine whether or not a flip over the 180 degree mark occurred
+int16_t tempOffset;
+int16_t rawDiff;
+int16_t lastRawDiff;
+int16_t rawOffset;
+int16_t lastRawOffset;
+int16_t raw[400];
+int16_t flip[400];
+uint16_t times[400];
+uint16_t flipThresh = 600;  // threshold to determine whether or not a flip over the 180 degree mark occurred
 bool flipped = false;
 
 int16_t main(void) {
     uint16_t btn2ReadState;
     uint16_t btn2CurrState = 0;
     uint16_t counter2;
+    uint16_t readTime;
 
     init_clock();
     init_timer();
@@ -53,44 +56,18 @@ int16_t main(void) {
     pin_set(&D[7]);
 
     pin_digitalOut(&D[6]);
-    oc_pwm(&oc1, &D[6], &timer2, 20000, 30000);
+    oc_pwm(&oc1, &D[6], &timer2, 20000, 60000);
 
-    lastLastRawPos = pin_read(&A[0]);
-    lastRawPos = pin_read(&A[0]);
+    timer_start(&timer1);
+
+    lastLastRawPos = pin_read(&A[0]) >> 6;
+    lastRawPos = pin_read(&A[0]) >> 6;
+
+    led_on(&led2);
 
     while(1){
+        led_off(&led1);
         btn2ReadState = !sw_read(&sw2);
-
-        rawPos = pin_read(&A[0]); 
-
-        rawDiff = rawPos - lastRawPos;          
-        lastRawDiff = rawPos - lastLastRawPos; 
-        rawOffset = abs(rawDiff);
-        lastRawOffset = abs(lastRawDiff);
-        
-        lastLastRawPos = lastRawPos;
-        lastRawPos = rawPos;
-          
-        if((lastRawOffset > flipThresh) && (!flipped)) { 
-            if(lastRawDiff > 0) {        
-                flipNumber--;             
-            } else {                     
-                flipNumber++;             
-            }
-            if(rawOffset > flipThresh) { 
-                updatedPos = rawPos + flipNumber*rawOffset; 
-                tempOffset = rawOffset;
-            } else {               
-                updatedPos = rawPos + flipNumber*lastRawOffset;  
-            }
-            flipped = true;          
-        } else {                       
-            updatedPos = rawPos + flipNumber*tempOffset;
-            flipped = false;
-        }
-
-        printf("%d   :   %d\n",rawPos,updatedPos);
-
         if (btn2CurrState != btn2ReadState) {
             counter2++;
         }
@@ -104,12 +81,45 @@ int16_t main(void) {
             btn2CurrState = btn2ReadState;
             if (btn2CurrState)
             {
-                oc_pwm(&oc1, &D[6], &timer2, 20000, 50000);
+                oc_pwm(&oc1, &D[6], &timer2, 0, 0);
             }
-            else
-            {
-                oc_pwm(&oc1, &D[6], &timer2, 20000, 30000);
+            // else
+            // {
+            //     oc_pwm(&oc1, &D[6], &timer2, 20000, 30000);
+            // }
+        }
+
+        for (int a = 0; a < 400; a++) {
+            for (int b = 0; b < 100; b++) {
+                rawPos = pin_read(&A[0]) >> 6;
+                readTime = timer_read(&timer1);
+
+                rawDiff = rawPos - lastRawPos;          
+                lastRawDiff = rawPos - lastLastRawPos; 
+                rawOffset = abs(rawDiff);
+                lastRawOffset = abs(lastRawDiff);
+                
+                lastLastRawPos = lastRawPos;
+                lastRawPos = rawPos;
+                  
+                if((lastRawOffset > flipThresh) && (!flipped)) { 
+                    if(lastRawDiff > 0) {        
+                        flipNumber--;             
+                    } else {                     
+                        flipNumber++;             
+                    }
+                    flipped = true;          
+                } else {                       
+                    flipped = false;
+                }
             }
+            raw[a] = rawPos;
+            flip[a] = flipNumber;
+            times[a] = readTime;
+        }
+        led_on(&led1);
+        for (int c = 0; c < 400; c++) {
+            printf("%d:%d;%d\n",raw[c],flip[c],times[c]);
         }
     }
 }
